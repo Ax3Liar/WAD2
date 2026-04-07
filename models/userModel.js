518e0ad28168970fb04364928d84639fc6fa8b73
Ax <tortejameso@gmail.com>
2026-04-07 09:31:38 +0100

    Added bcrypt in userModel and package, fixed mustache files (partials not included) like invalid syntax, missing contents and made some readable and some modifications

--- models/userModel.js
+++ models/userModel.js
@@ -1,10 +1,15 @@
 
 // models/userModel.js
+import bcrypt from 'bcryptjs';
 import { usersDb } from './_db.js';
 
 export const UserModel = {
   async create(user) {
-    return usersDb.insert(user);
+    const hashedPassword = await bcrypt.hash(user.password, 10);
+    return usersDb.insert({ ...user, password: hashedPassword });
+  },
+  async comparePassword(plain, hashed) {
+    return bcrypt.compare(plain, hashed);
   },
   async findByEmail(email) {
     return usersDb.findOne({ email });
@@ -13,4 +18,3 @@ export const UserModel = {
     return usersDb.findOne({ _id: id });
   }
 };
-``
